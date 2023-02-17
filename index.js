75b6e52db6b0cc324e6f35cd5f6a5262bd339763
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-02-17 18:52:52 +0000

    added dependecies package and README template

---
+++ index.js
@@ -0,0 +1,45 @@
+const inquirer = require('inquirer');
+
+inquirer
+.prompt
+
+
+
+
+`# ${response.projectName}
+
+## Description
+
+${response.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${response.installation}
+
+## Usage
+
+${response.usage}
+
+## Credits
+
+${response.credit}
+
+## License
+
+${response.usage}
+
+
+## How to Contribute
+
+${response.contributions}
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
